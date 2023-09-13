1598644e8d38f6294874235283268981796854a4
Juyoon Park <53207478+xper100@users.noreply.github.com>
2023-09-13 20:44:48 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_APP_NAME=jyp_container
+
+cd /home/ec2-user/jyp
+
+EXIST_CONTAINER=$(docker ps | grep $DOCKER_APP_NAME-main)
+
+if [ -z "$EXIST_CONTAINER" ]; then
+	docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
+
+	echo "Build Complete ${DOCKER_APP_NAME}-main container"
+fi
+
+docker system prune -af
