9152a534c5ad8f8f3165896ff8df5ad0cb57ed77
Evan-orderhero <xper100@gmail.com>
2023-09-15 12:28:37 +0900

    Update index.php

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
 echo '칭찬해 칭호받은 에반이올시다 제우스 팬서 콥 짱';
 
 
-$db_host = "host=evan.ca4xmd0qmadn.ap-northeast-2.rds.amazonaws.com"
+$db_host = "host=evan.ca4xmd0qmadn.ap-northeast-2.rds.amazonaws.com";
 $db_port = "port=5432";
 $db_user = "user=postgres";
 $db_password = "password=dpqks12345";
@@ -15,10 +15,10 @@ $conn = pg_connect($connection_info);
 $query = "select * from evan";
 $result = pg_query($conn, $query);
 
-$db_result = "";
+$db_result = [];
 while ($row = pg_fetch_assoc($result)) {
-    $db_result = $row['evan'];
+    $db_result[] = $row['evan'];
 }
 
 
-echo " ".$db_result;
+echo $db_result[0];
